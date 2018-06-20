58fa3750862d7d7748c6417a8c72e6ecdcd2c60c
John Lundgren <john.paul.lundgren@gmail.com>
2018-06-20 16:46:09 +0200

    added server functionality

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const RomanNumerals = require('./roman-numerals');
+const express = require('express');
+const app = express();
+const apiController = require('./controllers/apiController');
+const htmlController = require('./controllers/htmlController');
 
-let r = new RomanNumerals(155);
-console.log(r.convert());
+const port = process.env.port || 3000;
 
+app.use('/', (req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    next();
+});
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+apiController(app);
+htmlController(app);
+
+app.listen(port);
 
 
 
